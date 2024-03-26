356824e7afa2776613f55d5b3db86b66ba10aa59
cindymey08 <130966382+cindymey08@users.noreply.github.com>
2024-03-26 09:32:42 +0700

    commit

---
+++ lib/secrens/shopping_list_screen.dart
@@ -0,0 +1,17 @@
+import 'package:daftar_belanja/services/shopping_service.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart';
+
+class ShoppingListScreen extends StatefulWidget {
+  const ShoppingListScreen({super.key});
+
+  @override
+  State<ShoppingListScreen> createState() => _ShoppingListScreenState();
+}
+
+class _ShoppingListScreenState extends State<ShoppingListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
